8d0f861eb981c7d6d1bfeb44239214517b5dfce8
Mattufts <matthew.ebisu@tufts.edu>
2024-05-01 19:00:39 -0400

    updated some of the sentiment analysis code, to be analyzed further by matt

--- Emotion-Detection-master/train.py
+++ Emotion-Detection-master/train.py
@@ -115,5 +115,48 @@ history = model.fit_generator(
             validation_data=validation_generator,
             validation_steps=nb_validation_samples//batch_size)
 
+# import cv2
+# from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing.image import img_to_array
+# from tensorflow.keras.layers import Input
+# from tensorflow.keras.models import Model
 
 
+# input_img = Input(shape=(48, 48, 1))  # For grayscale images
+
+# # Load model and haarcascade for face detection
+# model = load_model('/home/tadashi_e/Documents/GithubRepos/CrazieFlieSwarmCS141/Emotion-Detection-master/Emotion_Detection.h5')
+# face_classifier = cv2.CascadeClassifier('/home/tadashi_e/Documents/GithubRepos/CrazieFlieSwarmCS141/Emotion-Detection-master/haarcascade_frontalface_default.xml')
+
+# # Start video capture
+# cap = cv2.VideoCapture(0)
+
+# while True:
+#     ret, frame = cap.read()
+#     if not ret:
+#         break
+    
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    
+#     for (x, y, w, h) in faces:
+#         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+#         roi_gray = gray[y:y+h, x:x+w]
+#         roi_gray = cv2.resize(roi_gray, (48, 48))
+#         roi = roi_gray.astype('float') / 255.0
+#         roi = img_to_array(roi)
+#         roi = np.expand_dims(roi, axis=0)
+        
+#         # Predicting the emotion
+#         preds = model.predict(roi)[0]
+#         emotion_probability = np.max(preds)
+#         label = str(preds.argmax())
+        
+#         cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+
+#     cv2.imshow('Emotion Detector', frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+# cap.release()
+# cv2.destroyAllWindows()
